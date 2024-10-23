0068383e16e7fcb6d1cc3b04c32d15d8ec167133
Güven Fazlı <guven.fazli@gmail.com>
2024-10-23 04:27:28 +0300

    Routers and Controllers created

--- fullstack-cms-app/backend/index.js
+++ fullstack-cms-app/backend/index.js
@@ -2,12 +2,15 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser')
 const sequelize = require('./utils/database.js')
-
 // M O D E L S 
 const Employee = require('./models/Employee.js')
 const Admin = require('./models/Admin.js')
 
+// R O U T E R S 
 
+const authRouter = require('./routes/authRoute.js')
+
+// M I D D L E W A R E S 
 
 app.use(bodyParser.json()) // application/json
 
@@ -18,7 +21,7 @@ app.use((req, res, next) => {
   next();
 })
 
-
+app.use('/auth', authRouter)
 
 
 
@@ -31,10 +34,6 @@ Admin.belongsTo(Employee, { onDelete: 'CASCADE', foreignKey: 'employeeId' })
 
 
 sequelize.sync().then(async res => {
-
-
-
-
 
   app.listen(8080)
 }).catch(err => console.log(err));
