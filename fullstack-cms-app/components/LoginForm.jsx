d19e5236dd32bdd20919d9a5b18aff6708d84f01
Güven Fazlı <guven.fazli@gmail.com>
2024-10-23 04:02:56 +0300

    Paddings added

--- fullstack-cms-app/components/LoginForm.jsx
+++ fullstack-cms-app/components/LoginForm.jsx
@@ -3,13 +3,13 @@ import Link from "next/link"
 export default function LoginForm() {
   return (
     <div className="flex flex-col items-center w-2/6 max-lg:w-2/4 max-sm:w-4/5">
-      <form className="flex flex-col text-xl p-4 w-full bg-gray-900 rounded-lg shadow-2xl max-lg:text-base max-sm:text-sm">
-        <label htmlFor="email" className=" mb-3">Email Address</label>
+      <form className="flex flex-col text-xl p-4 px-8 w-full bg-gray-900 rounded-lg shadow-2xl max-lg:text-base max-sm:text-sm">
+        <label htmlFor="email" className="mb-3">Email Address</label>
         <input name="email" id="email" className="text-gray-800 px-1 py-2 mb-3 rounded-md pl-2" placeholder="Enter Your Username"></input>
         <label htmlFor="password" className=" mb-3">Password</label>
         <input name="password" id="password" className="text-gray-800 px-1 py-2 rounded-md pl-2 mb-5" type="password" placeholder="Enter Your Password"></input>
         <div className="flex text-xl w-full items-center justify-between max-xl:flex-col max-xl:text-center max-lg:text-base max-lg:flex-col max-lg:text-center max-sm:text-sm">
-          <button className="font-bold hover:text-gray-300 duration-150 max-xl:mb-5 max-lg:mb-5">Login</button>
+          <button className="font-bold border border-gray-800 px-5 py-2 rounded-lg hover:text-gray-900 hover:bg-gray-500 duration-150 max-xl:mb-5 max-lg:mb-5">Login</button>
           <Link className="text-gray-400 hover:text-gray-300 duration-100" href={'/userRegister'}>Don't You Have an Account? <span className="font-bold">Create One!</span></Link>
         </div>
       </form>
--- fullstack-cms-app/components/RegisterForm.jsx
+++ fullstack-cms-app/components/RegisterForm.jsx
@@ -2,20 +2,20 @@ import Link from "next/link"
 
 export default function LoginForm() {
   return (
-    <div className="flex flex-col items-center w-2/6">
-      <form className="flex flex-col  p-4 w-full bg-gray-900 rounded-lg shadow-2xl">
-        <label htmlFor="name" className="text-xl mb-3">Name</label>
-        <input name="name" id="name" className="text-gray-800 text-xl p-1 mb-3 rounded-md pl-2" placeholder="Enter Your Name"></input>
-        <label htmlFor="surname" className="text-xl mb-3">Surname</label>
-        <input name="surname" id="surname" className="text-gray-800 text-xl p-1 mb-3 rounded-md pl-2" placeholder="Enter Your Surname"></input>
-        <label htmlFor="email" className="text-xl mb-3">Email Address</label>
-        <input name="email" id="email" className="text-gray-800 text-xl p-1 mb-3 rounded-md pl-2" placeholder="Enter Your Username"></input>
-        <label htmlFor="password" className="text-xl mb-3">Password</label>
-        <input name="password" id="password" className="text-gray-800 text-xl p-1 rounded-md pl-2 mb-5" type="password" placeholder="Enter Your Password"></input>
-        <label htmlFor="jobTitle" className="text-xl mb-3">Job Title</label>
-        <input name="jobTitle" id="jobTitle" className="text-gray-800 text-xl p-1 mb-3 rounded-md pl-2" placeholder="Title"></input>
-        <div className="flex w-full items-center justify-between">
-          <button className="text-xl font-bold  hover:text-gray-300 duration-150">Create Account</button>
+    <div className="flex flex-col items-center w-2/6 max-lg:w-2/4 max-sm:w-4/5">
+      <form className="flex flex-col text-xl py-6 px-8 w-full bg-gray-900 rounded-lg shadow-2xl max-lg:text-base max-sm:text-sm">
+        <label htmlFor="name" className="mb-3">Name</label>
+        <input name="name" id="name" className="text-gray-800 p-1 mb-3 rounded-md pl-2" placeholder="Enter Your Name"></input>
+        <label htmlFor="surname" className="mb-3">Surname</label>
+        <input name="surname" id="surname" className="text-gray-800 p-1 mb-3 rounded-md pl-2" placeholder="Enter Your Surname"></input>
+        <label htmlFor="email" className="mb-3">Email Address</label>
+        <input name="email" id="email" className="text-gray-800 p-1 mb-3 rounded-md pl-2" placeholder="Enter Your Username"></input>
+        <label htmlFor="password" className="mb-3">Password</label>
+        <input name="password" id="password" className="text-gray-800 p-1 rounded-md pl-2 mb-5" type="password" placeholder="Enter Your Password"></input>
+        <label htmlFor="jobTitle" className="mb-3">Job Title</label>
+        <input name="jobTitle" id="jobTitle" className="text-gray-800 p-1 mb-3 rounded-md pl-2" placeholder="Title"></input>
+        <div className="flex text-xl w-full items-center justify-between max-xl:flex-col max-xl:text-center max-lg:text-base max-lg:flex-col max-lg:text-center max-sm:text-sm">
+          <button className="border border-gray-800 px-5 py-2 rounded-lg font-bold hover:text-gray-900 hover:bg-gray-500 duration-150">Create Account</button>
           <Link className="text-gray-400 hover:text-gray-300 duration-100" href={'/userLogin'}>Do You Have an Account? <span className="font-bold">Login</span></Link>
         </div>
       </form>
