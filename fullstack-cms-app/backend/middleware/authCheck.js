84ea3995c8c2911ff377bfd0d989fba6a937664b
Güven Fazlı <guven.fazli@gmail.com>
2024-10-27 00:24:21 +0300

    Route protection is done

---
+++ fullstack-cms-app/backend/middleware/authCheck.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken')
+
+module.exports = (req, res, next) => {
+  try {
+    const cookie = req.cookies['jwt']
+
+    const resolvedCookie = jwt.decode(cookie)
+
+    if (!resolvedCookie) {
+      const error = new Error('Please log in first!')
+      error.statusCode = 402
+      throw error
+    }
+
+    next()
+
+  } catch (err) {
+    next(err)
+  }
+}
--- fullstack-cms-app/backend/routes/userRoute.js
+++ fullstack-cms-app/backend/routes/userRoute.js
@@ -1,8 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController')
+const authCheck = require('../middleware/authCheck')
 const { body } = require('express-validator')
 
-router.get('/home', userController.fetchUserData)
+router.get('/home', authCheck, userController.fetchUserData)
 
 module.exports = router
