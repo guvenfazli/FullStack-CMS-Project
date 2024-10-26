3ec3a9c174af133765def24ce290962424aa915a
Güven Fazlı <guven.fazli@gmail.com>
2024-10-26 22:13:42 +0300

    Toggle for NavBar menu completed

--- fullstack-cms-app/components/NavBar/NavBar.jsx
+++ fullstack-cms-app/components/NavBar/NavBar.jsx
@@ -1,11 +1,24 @@
 import { useState } from "react"
+import { useAppContext } from "@/context"
+import { Switch } from "../ui/switch"
+import { Label } from "../ui/label"
 import Dashboard from "./Dashboard"
 import Resources from "./Resources"
-import { useAppContext } from "@/context"
+
 export default function NavBar() {
   const { isLogged } = useAppContext()
   const [isDashBoard, setIsDashBoard] = useState(false)
   const [isResources, setIsResources] = useState(false)
+
+  function toggleAll() {
+    if (isDashBoard !== isResources) {
+      setIsDashBoard(true)
+      setIsResources(true)
+    } else {
+      setIsDashBoard(prev => !prev)
+      setIsResources(prev => !prev)
+    }
+  }
 
   if (isLogged) {
     return (
@@ -18,12 +31,17 @@ export default function NavBar() {
           <Dashboard isOpen={isDashBoard} />
         </div>
 
-        <div>
+        <div className="mb-3">
           <div onClick={() => setIsResources(prev => !prev)} className="flex items-start justify-between hover:cursor-pointer">
             <button className={`${isResources && "text-gray-600"} duration-100 hover:text-gray-500`}>Resources</button>
             <button className={`${!isResources ? 'rotate-0' : 'rotate-90 text-gray-600'} duration-100 hover:text-gray-500`}>{!isResources ? '+' : 'x'}</button>
           </div>
           <Resources isOpen={isResources} />
+        </div>
+
+        <div className="flex items-center flex-row">
+          <Switch onCheckedChange={() => toggleAll()} className="mr-1" id="toggle-all" />
+          <Label htmlFor="toggle-all">Toggle All</Label>
         </div>
       </div>
     )
