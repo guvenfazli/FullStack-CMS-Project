b799c6e63966706840726883c75d2fb073ca908c
Güven Fazlı <guven.fazli@gmail.com>
2024-11-01 06:15:45 +0300

    Grouping in tailwind is done.

--- fullstack-cms-app/components/AssignEmployees/EmployeeCard.jsx
+++ fullstack-cms-app/components/AssignEmployees/EmployeeCard.jsx
@@ -13,8 +13,8 @@ export default function EmployeeCard({ employee, chooseEmployee, removeEmployee,
 
 
   return (
-    <div onClick={isChosen ? () => removeEmployee(employee.id) : () => chooseEmployee(employee.id)} className="flex flex-col hover:cursor-pointer justify-start items-center">
-      <Avatar className="duration-75 mb-2 w-20 h-20 hover:border-4 hover:border-yellow-400 hover:cursor-pointer">
+    <div onClick={isChosen ? () => removeEmployee(employee.id) : () => chooseEmployee(employee.id)} className="flex flex-col group hover:cursor-pointer justify-start items-center">
+      <Avatar className="duration-75 mb-2 w-20 h-20 group-hover:border-4 group-hover:border-yellow-400 group-hover:cursor-pointer">
         <div className={`text-center duration-100 flex justify-center items-center absolute border z-30 top-0 w-full bg-slate-300 rounded-full bottom-0 ${isChosen ? 'opacity-50' : 'opacity-0'}`}>
           {chosenIcon}
         </div>
