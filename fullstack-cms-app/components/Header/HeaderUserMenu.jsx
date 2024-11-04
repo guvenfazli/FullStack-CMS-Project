c61ccb9fbab4fa27b413c6635f372601536408b0
Güven Fazlı <guven.fazli@gmail.com>
2024-11-04 03:35:08 +0300

    Routing fixed.

--- fullstack-cms-app/components/Header/HeaderUserMenu.jsx
+++ fullstack-cms-app/components/Header/HeaderUserMenu.jsx
@@ -14,8 +14,8 @@ export default function HeaderUserMenu({ isMenu, user, setIsMenu }) {
 
   return (
     <div onMouseLeave={() => setIsMenu(false)} className={`${!isMenu ? 'opacity-0 invisible' : 'opacity-100'} flex flex-col items-start bg-gray-700 rounded-md px-4 py-2 absolute right-5 top-14 duration-100 `}>
-      {user && <Link className="mb-2 hover:text-gray-500 duration-100" href={`/users/${user.userId}`}>Profile</Link>}
-      <Link href={`/users/${user.userId}`} className="mb-2 hover:text-gray-500 duration-100">My Tasks</Link>
+      {user && <Link className="mb-2 hover:text-gray-500 duration-100" href={`/employees/${user.userId}`}>Profile</Link>}
+      <Link href={`/employees/${user.userId}`} className="mb-2 hover:text-gray-500 duration-100">My Tasks</Link>
       <button className="hover:text-gray-500 duration-100" onClick={() => signOut()}>Log Out</button>
     </div>
   )
--- fullstack-cms-app/components/SingleEmployee/Task.jsx
+++ fullstack-cms-app/components/SingleEmployee/Task.jsx
@@ -2,7 +2,7 @@ import UserTaskStatus from "../HomePage/UserTable/UserTaskStatus"
 import Link from "next/link"
 export default function Task({ task }) {
   return (
-    <Link href={`/projects/${task.projectId}`} className="flex w-full justify-between items-center border-b border-b-gray-500 p-2">
+    <Link href={`/projects/${task.projectId}`} className="flex w-full justify-between items-center border-b border-b-gray-500 p-2 hover:bg-slate-500 duration-100">
       <p className="text-lg">{task.taskName}</p>
       <UserTaskStatus status={task.taskStatus}>{task.taskStatus}</UserTaskStatus>
     </Link>
