6467be1b8a859444d2e6e2d851cca7e3f6e5d7c2
Güven Fazlı <guven.fazli@gmail.com>
2024-11-05 07:48:02 +0300

    Added edit icon, will apply the design later.

--- fullstack-cms-app/components/SingleEmployee/SingleEmployeeCard.jsx
+++ fullstack-cms-app/components/SingleEmployee/SingleEmployeeCard.jsx
@@ -1,12 +1,26 @@
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar"
-import { mailIcon, employeeRoleIcon, dateIcon, phoneIcon, profileTaskIcon, completedTask } from "../Icons/Icons"
+import { mailIcon, employeeRoleIcon, dateIcon, phoneIcon, profileTaskIcon, completedTask, editProfileIcon } from "../Icons/Icons"
+import { useAppContext } from "@/context"
 import SingleEmployee from "./SingleEmployee"
 import dateFormatter from "@/utils/dateFormatter"
+import { useState } from "react"
+
 export default function SingleEmployeeCard({ fetchedEmployee }) {
 
+  const { isLogged } = useAppContext()
+
   return (
-    <div className="flex flex-col shadow-md shadow-slate-950 w-full rounded-lg bg-gray-900">
-      <div className="flex flex-col justify-start items-center py-3 px-4">
+    <div className={`flex flex-col rotate-0 shadow-md shadow-zinc-900 duration-200 w-full rounded-lg bg-gray-900`}>
+      {
+        isLogged?.userId === fetchedEmployee?.id &&
+
+        <div className="flex w-full pt-4 pr-4 items-center justify-end">
+          <button onClick={() => setIsEdit(prev => !prev)}>{editProfileIcon}</button>
+        </div>
+      }
+
+      <div className={`flex flex-col relative justify-start items-center py-3 overflow-hidden px-4`}>
+
         <div>
           <Avatar className="w-40 h-40 border-yellow-600 border-4">
             <AvatarImage src={`http://localhost:8080/${fetchedEmployee?.profilePic}`} />
@@ -40,15 +54,19 @@ export default function SingleEmployeeCard({ fetchedEmployee }) {
         </div>
 
 
-        <div className="flex w-full items-center gap-5 p-3 justify-start">
-          <p>{profileTaskIcon}</p>
-          <SingleEmployee>{fetchedEmployee?.tasks.length}</SingleEmployee>
+        <div className="flex w-full items-center gap-5 p-3 justify-between">
+          <div className="flex items-center gap-2">
+            <p>{profileTaskIcon}</p>
+            <SingleEmployee>{fetchedEmployee?.tasks.length}</SingleEmployee>
+          </div>
+
+          <div className="flex items-center gap-2">
+            <p>{completedTask}</p>
+            <SingleEmployee>{fetchedEmployee?.completedTasks}</SingleEmployee>
+          </div>
         </div>
 
-        <div className="flex w-full items-center gap-5 p-3 justify-start">
-          <p>{completedTask}</p>
-          <SingleEmployee>{fetchedEmployee?.completedTasks}</SingleEmployee>
-        </div>
+
 
         <div className="flex w-full justify-center items-center text-center">
           <p className="text-sm text-gray-500">Joined at {dateFormatter(fetchedEmployee?.createdAt)}</p>
