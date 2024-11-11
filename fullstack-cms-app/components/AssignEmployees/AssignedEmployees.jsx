bd86344823ffa1d4b3dac22518bd6a2e6e403fad
Güven Fazlı <guven.fazli@gmail.com>
2024-11-11 20:43:17 +0300

    .

--- fullstack-cms-app/components/AssignEmployees/AssignedEmployees.jsx
+++ fullstack-cms-app/components/AssignEmployees/AssignedEmployees.jsx
@@ -5,7 +5,7 @@ export default function AssignedEmployees({ task, employee, resignEmployees }) {
     <div onClick={() => resignEmployees(task.id, employee.id)}>
       <Avatar className="duration-75 mb-2 hover:border-4 hover:border-yellow-400 hover:cursor-pointer">
         <AvatarImage className="z-10" src={`http://localhost:8080/${employee.profilePic}`} />
-        <AvatarFallback>PP</AvatarFallback>
+        <AvatarFallback>P</AvatarFallback>
       </Avatar>
     </div>
   )
