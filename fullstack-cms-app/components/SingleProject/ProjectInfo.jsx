13606309e0d7d27a99a441abed912bb49d909835
Güven Fazlı <guven.fazli@gmail.com>
2024-11-14 09:54:05 +0300

    Style added.

--- fullstack-cms-app/components/SingleProject/ProjectInfo.jsx
+++ fullstack-cms-app/components/SingleProject/ProjectInfo.jsx
@@ -1,6 +1,6 @@
 export default function ProjectInfo({ title, children }) {
   return (
-    <div className="py-3 px-1 w-2/3 flex justify-between items-center">
+    <div className="py-3 px-1 w-full flex justify-between items-center">
       <p className="text-xl">{title}</p>
       <p className="text-xl">{children}</p>
     </div>
