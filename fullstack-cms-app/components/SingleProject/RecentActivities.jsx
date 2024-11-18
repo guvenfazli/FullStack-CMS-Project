05f8cd5453bc3620b0ec7769905d383c430ec6c9
Güven Fazlı <guven.fazli@gmail.com>
2024-11-18 03:09:04 +0300

    Visual bug fixed for recent activities and assigned employees

--- fullstack-cms-app/components/SingleProject/RecentActivities.jsx
+++ fullstack-cms-app/components/SingleProject/RecentActivities.jsx
@@ -3,9 +3,9 @@ import Activity from "./Activity"
 export default function RecentActivities({ projectActivities }) {
 
   return (
-    <div className="flex p-2 flex-col gap-2 h-1/2 justify-start">
+    <div className="flex p-2 flex-col gap-2  h-1/2 justify-start">
       <div className="w-full py-2 border-t border-b border-yellow-600 flex justify-center items-center">
-        <p className="text-xl max-sm:text-base">Recent Activities</p>
+        <p className="text-xl">Recent Activities</p>
       </div>
 
       {projectActivities && <Activity projectActivities={projectActivities} />}
