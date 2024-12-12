38e2bdfa59029d44266063d946eca03b1dc5384f
Güven Fazlı <guven.fazli@gmail.com>
2024-12-12 07:20:59 +0300

    Comment added

--- fullstack-cms-app/utils/deadLineChecker.js
+++ fullstack-cms-app/utils/deadLineChecker.js
@@ -4,7 +4,7 @@ export default function deadLineChecker(date) { // Calculating remaining time.
   const fixedDate = new Date(date)
   const fixedTime = fixedDate.getTime()
   const remainingDay = fixedTime - today
-  const formatRemainingDay = Math.round(remainingDay / (1000 * 3600 * 24))
+  const formatRemainingDay = Math.round(remainingDay / (1000 * 3600 * 24)) // calculating the remaining days for the deadline.
   if (formatRemainingDay <= 3 && formatRemainingDay > 0) {
     return formatRemainingDay + " days left for the project!"
   } else if (formatRemainingDay === 0) {
